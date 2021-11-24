c0713e986ab84fdafdeb44b5d578d0b922ae5043
Robert Yokabaskas <robertyokabaskas@Roberts-MacBook-Air.local>
2021-11-24 12:11:43 -0500

    Adding Basic Maps

---
+++ Mapping_GeoJSON/static/js/logic.js
@@ -0,0 +1,25 @@
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
+let airportData = 
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
+
+
+
+
